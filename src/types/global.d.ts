24a1a2f28d59b4d3dbc16453106abd2597c9aeb5
Vulps <ajmcallister5@gmail.com>
2025-11-12 20:46:40 +0000

    #5 - Add buttons collection to global types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,10 +1,11 @@
 import { Client, Collection } from 'discord.js';
-import { Command } from '../utils';
+import { ButtonHandler, Command } from '../utils';
 import { Config } from '../config';
 
 declare global {
     var client: Client;
     var commands: Collection<string, Command>;
+    var buttons: Collection<string, ButtonHandler>;
     var config: typeof Config;
 }
 
