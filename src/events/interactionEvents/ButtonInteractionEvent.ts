81916a88320dbf2b1b9ef5f80a71cbb6e4a11881
Vulps <ajmcallister5@gmail.com>
2025-11-20 03:13:03 +0000

    #5 - Update ButtonInteractionEvent for ban reasons

--- src/events/interactionEvents/ButtonInteractionEvent.ts
+++ src/events/interactionEvents/ButtonInteractionEvent.ts
@@ -1,12 +1,12 @@
 import { ButtonInteraction } from "discord.js";
 import { InteractionEvent } from "./InteractionEvent";
-import { ButtonHandler, Logger } from "../../utils";
+import { Handler, Logger } from "../../utils";
 import { BotButtonInteraction } from "../../structures";
 
 class ButtonInteractionEvent implements InteractionEvent<ButtonInteraction> {
     async execute(interaction: ButtonInteraction, executionId: string) {
         const botInteraction = new BotButtonInteraction(interaction, executionId)
-        const button: ButtonHandler | undefined = global.buttons.get(botInteraction.baseId);
+        const button: Handler<BotButtonInteraction> | undefined = global.buttons.get(botInteraction.baseId);
         if(!button) {
             Logger.error(`Button not found for Custom ID: ${botInteraction.baseId}`);
             return;
