2c25ce8e9e74aea132824a0d38788e9f43c24e76
Vulps <ajmcallister5@gmail.com>
2025-11-26 22:39:15 +0000

    #5 - Add user interfaces

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,1 +1,4 @@
 export { Question } from './QuestionInterface';
+export { TrackingCacheEntry } from './TrackingCacheEntry';
+export { User } from './UserInterface';
+export { UserProfile } from './UserProfileInterface';
