03fe4affb13e21e800760e44140b928305d1f0fd
Vulps <ajmcallister5@gmail.com>
2025-12-30 15:50:30 +0000

    #7 - Fix export statement for questionEmbed

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,5 +1,5 @@
 // Barrel file for views
 export {confirmNewQuestionEmbed} from './question_views/confirmNewQuestionEmbed';
-export {questionEmbed} from './question_views/questionEmbed';
 export {newQuestionView} from './moderation/newQuestionView';
+export {questionEmbed} from './question_views/questionEmbed';
 export {userProfileView} from './moderation/UserProfileView';
