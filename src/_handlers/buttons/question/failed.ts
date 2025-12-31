f20d996f2655246a1699c3516adce919bb22d6e1
Vulps <ajmcallister5@gmail.com>
2025-12-31 17:45:17 +0000

    #17 - Fix "read but never used" error blocking bot build

--- src/_handlers/buttons/question/failed.ts
+++ src/_handlers/buttons/question/failed.ts
@@ -4,7 +4,9 @@ import { Handler } from '../../../utils';
 const failed: Handler<BotButtonInteraction> = {
     name: 'failed',
     async execute(interaction: BotButtonInteraction): Promise<void> {
-        const questionId = interaction.params.get('id');        
+        const questionId = interaction.params.get('id');
+        console.log(`Question ID marked as failed: ${questionId}`); // This place holder allows the bot to run before we implement the full function 
+
         await interaction.ephemeralReply('❌ Marked as FAILED!');
     }
 };
