0e24290e557d098c70ac65bfebde7e7bdf339870
Vulps <ajmcallister5@gmail.com>
2026-03-14 01:58:34 +0000

    #6 - Add server view

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,6 +1,7 @@
 // Barrel file for views
 export {confirmNewQuestionEmbed} from './question_views/confirmNewQuestionEmbed';
 export {newQuestionView} from './moderation/newQuestionView';
+export {serverView} from './moderation/serverView';
 export {userProfileView} from './moderation/UserProfileView';
 export {questionEmbed} from './question_views/questionEmbed';
 export {default as helpView} from './util/helpView';
