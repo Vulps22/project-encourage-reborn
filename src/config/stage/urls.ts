685d6c8cd50f8271ef99a34720e5e005d13cd8cf
Vulps <ajmcallister5@gmail.com>
2026-04-14 15:53:00 +0100

    Add MS_URL to all environment configs

--- src/config/stage/urls.ts
+++ src/config/stage/urls.ts
@@ -5,4 +5,5 @@ export const Urls = {
     WIKI: 'http://github.com/Vulps22/truth-or-dare-discord-bot/wiki/',
     ADD_BOT: 'https://discord.com/oauth2/authorize?client_id=1079207025315164331',
     DS_URL: 'https://ds.stage.vulps.co.uk/api/v1',
+    MS_URL: 'https://ms.stage.vulps.co.uk',
 } as const;
